14b9d4c8455768439f5032c702c07922d2234d67
O9server <katramadoss@gmail.com>
2022-11-01 14:00:22 +0200

    fixing 500 error

--- pages/pastes/[id].vue
+++ pages/pastes/[id].vue
@@ -5,7 +5,7 @@
             <p>Author: <span>{{ paste.author }}</span></p>
             <div id="paste">
                 <!-- <p>{{paste.syntax}}</p> -->
-                <prism v-if="loadSyntax" id="prism" :language="paste.syntax">{{paste.paste}}</prism>
+                <!-- <prism v-if="loadSyntax" id="prism" :language="paste.syntax">{{paste.paste}}</prism> -->
                 <!-- <textarea readonly>{{paste.paste}}</textarea>
                 <textarea readonly>{{paste.syntax}}</textarea> -->
             </div>
@@ -37,8 +37,11 @@ useHead({
     ]
 });
 
+onBeforeMount(() => {
+    console.log(paste.value.syntax);
+})
+
 onMounted(() => {
-    console.log('eeee');
     console.log(paste.value.syntax);
     // console.log(route.params.id);
     setTimeout(() => {
