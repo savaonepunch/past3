0d48902d0e08e914bbbc8aa5dc63bfc29fcc1137
O9server <katramadoss@gmail.com>
2022-11-01 20:39:10 +0200

    fixing 500 error

--- app.vue
+++ app.vue
@@ -11,7 +11,7 @@
 <script setup>
 import 'prismjs'
 import 'prismjs/components/prism-clike'
-// import 'prismjs/components/prism-java'
+import 'prismjs/components/prism-java'
 import 'prismjs/components/prism-c'
 import 'prismjs/components/prism-cpp'
 
